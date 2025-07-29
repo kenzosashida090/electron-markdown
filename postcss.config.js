b2421be1414737e282323f709f319bb882ed6502
kenzosashida090 <kenzosashida1@gmail.com>
2025-07-29 14:41:10 -0600

    [Fix] Change tailwind postcss config

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  plugins: {
-    tailwindcss: {}, // This enables Tailwind CSS processing
-    autoprefixer: {}, // This adds vendor prefixes to your CSS
-  },
+  plugins: [
+    require('@tailwindcss/postcss')(), 
+    require('autoprefixer'),
+  ],
 };
